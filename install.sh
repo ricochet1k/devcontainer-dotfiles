4802ef8b60d5ed99ba874ee2b7edde12d7e47907
Matt Peterson <mpeterson@privacy.com>
2021-10-22 14:09:48 -0600

    install curl if needed

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! which curl ; then
+    if which apt ; then
+        apt update
+        apt install curl
+    fi
+fi
+
 if ! which starship ; then
     sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
     echo 'eval "$(starship init bash)"' >> ~/.bashrc
